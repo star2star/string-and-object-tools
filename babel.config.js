8ca7e1251fd8bd9a4997e2d0b23092e08fb6d0c9
Nathan Harris <nharris@star2star.com>
2019-07-10 14:23:03 -0400

    update documentation and babel

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,7 @@
 const presets = [
-  [
-    "@babel/env",
-    {
-      targets: {
-        edge: "17",
-        firefox: "60",
-        chrome: "67",
-        safari: "11.1",
-        node: "8.0.0"
-      },
-      useBuiltIns: "usage",
-    },
-  ],
+    [
+      '@babel/preset-env'
+    ],
 ];
 
 module.exports = { presets };
